5603021d21810ab7c4d2dff7f0813e2d2961aa21
Jasper <j.bonefaas@student.avans.nl>
2019-10-07 15:53:24 +0100

    Added message handling frame

---
+++ BKnE2Client/client/controller/Controller.cs
@@ -0,0 +1,27 @@
+﻿using BKnE2Client.client.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKnE2Client.client.controller
+{
+    class Controller
+    {
+        private MessageHandler messageHandler;
+        private NetworkConnection networkConnection;
+
+        public Controller()
+        {
+            this.messageHandler = new MessageHandler(this);
+            this.networkConnection = new NetworkConnection(this);
+            this.networkConnection.Connect(ClientConfig.host, ClientConfig.port);
+        }
+
+        public void HandleMessage(string message)
+        {
+            messageHandler.Invoke(message);
+        }
+    }
+}
---
+++ BKnE2Client/client/model/NetworkConnection.cs
@@ -0,0 +1,50 @@
+﻿using BKnE2Client.client.controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKnE2Client.client.model
+{
+    class NetworkConnection
+    {
+        private NetworkStream stream;
+        private byte[] buffer = new byte[1024];
+        private Controller controller;
+        private Encoding encoding = Encoding.UTF8;
+
+        public NetworkConnection(Controller controller)
+        {
+            this.controller = controller;
+        }
+               
+        //Connect to the server
+        public void Connect(string ip, int port)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            stream = client.GetStream();
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+        }
+
+        //Read the message
+        private void OnRead(IAsyncResult ar)
+        {
+            int receivedBytes = stream.EndRead(ar);
+            string message = encoding.GetString(buffer, 0, receivedBytes);
+
+            controller.HandleMessage(message);
+
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+        }
+
+        //Send a message to the server
+        public void Write(string message)
+        {
+            stream.Write(encoding.GetBytes(message), 0, message.Length);
+            stream.Flush();
+        }
+    }
+}
