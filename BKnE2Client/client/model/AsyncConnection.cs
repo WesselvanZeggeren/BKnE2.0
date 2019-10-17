0d465e63de30d579b562a81b920e7b125421f56b
Jasper <j.bonefaas@student.avans.nl>
2019-10-17 17:10:41 +0200

    Added try catch for IOexceptions

--- BKnE2Client/client/model/AsyncConnection.cs
+++ BKnE2Client/client/model/AsyncConnection.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Security;
 using System.Text;
@@ -26,38 +27,51 @@ namespace BKnE2Client.client.model
 
         private void OnRead(IAsyncResult ar)
         {
-            
-            int receivedBytes = this.stream.EndRead(ar);
-            int packetLength = (int)buffer[0]; //BitConverter.ToInt32(buffer, 0);
-            byte[] totalBuffer = new byte[packetLength];
-            int readPos = 1;
-
-            while (readPos < packetLength)
+            try
             {
-                receivedBytes = stream.Read(totalBuffer, readPos, packetLength - readPos);
-                readPos += receivedBytes;
+                int receivedBytes = this.stream.EndRead(ar);
+                int packetLength = (int)buffer[0]; //BitConverter.ToInt32(buffer, 0);
+                byte[] totalBuffer = new byte[packetLength];
+                int readPos = 1;
+
+                while (readPos < packetLength)
+                {
+                    receivedBytes = stream.Read(totalBuffer, readPos, packetLength - readPos);
+                    readPos += receivedBytes;
+                }
+
+                string message = Encoding.UTF8.GetString(totalBuffer);
+
+                Request request = JsonConvert.DeserializeObject<Request>(message);
+
+                this.conn.receiveRequest(request);
+                this.stream.BeginRead(this.buffer, 0, buffer.Length, OnRead, null);
             }
-
-            string message = Encoding.UTF8.GetString(totalBuffer);
-
-            Request request = JsonConvert.DeserializeObject<Request>(message);
-
-            this.conn.receiveRequest(request);
-            this.stream.BeginRead(this.buffer, 0, buffer.Length, OnRead, null);
+            catch (IOException)
+            {
+                stream.Close();
+            }
         }
 
         public void Write(Request request)
         {
-            byte[] messageBytes = encoding.GetBytes(JsonConvert.SerializeObject(request));
-            byte[] bytes = new byte[messageBytes.Length + 1];
+            try
+            {
+                byte[] messageBytes = encoding.GetBytes(JsonConvert.SerializeObject(request));
+                byte[] bytes = new byte[messageBytes.Length + 1];
 
-            bytes[0] = (byte)bytes.Length;
+                bytes[0] = (byte)bytes.Length;
 
-            for (int i = 0; i < messageBytes.Length; i++)
-                bytes[i + 1] = messageBytes[i];
+                for (int i = 0; i < messageBytes.Length; i++)
+                    bytes[i + 1] = messageBytes[i];
 
-            stream.Write(bytes, 0, bytes.Length);
-            stream.Flush();
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+            }
+            catch (ObjectDisposedException)
+            {
+                
+            }
         }
     }
 }
